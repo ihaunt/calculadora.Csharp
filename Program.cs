5d3f20869b5b6f1a6770d4eac34e86fc0f497c89
Jennifer Machado <116113425+ihaunt@users.noreply.github.com>
2023-06-04 16:26:04 -0300

    Alteração com varificação
    ok

--- Program.cs
+++ Program.cs
@@ -1,14 +1,23 @@
 ﻿using System;
 using Calculadora;
 
+
 Calcular calculadora = new Calcular();
 
-Console.WriteLine("Digite o numero referente a operação que deseja realizar:");
-Console.WriteLine(" 1 - Adição");
-Console.WriteLine(" 2 - Substração");
-Console.WriteLine(" 3 - Divisão");
-Console.WriteLine(" 4 - Multiplicação");
-int operacao = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Digite o número referente à operação que deseja realizar:");
+Console.WriteLine("1 - Adição");
+Console.WriteLine("2 - Subtração");
+Console.WriteLine("3 - Divisão");
+Console.WriteLine("4 - Multiplicação");
+int operacao = 0;
+
+bool operacaoValida = false;
+while (!operacaoValida)
+{
+Console.WriteLine("Digite um número válido: 1, 2, 3 ou 4");
+bool inputValido = int.TryParse(Console.ReadLine(), out operacao);
+operacaoValida = inputValido && (operacao >= 1 && operacao <= 4);
+}
 
 Console.WriteLine("Digite o primeiro número:");
 int num1 = Convert.ToInt32(Console.ReadLine());
